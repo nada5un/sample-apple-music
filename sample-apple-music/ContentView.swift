02a491d943f0db996b4ba279264bd74281dff638
daeun <sung980524@naver.com>
2024-01-12 16:53:08 +0900

    Initial Commit

---
+++ sample-apple-music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample-apple-music
+//
+//  Created by 성다은 on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample-apple-music/sample_apple_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_apple_musicApp.swift
+//  sample-apple-music
+//
+//  Created by 성다은 on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct sample_apple_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
